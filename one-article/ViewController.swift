39ee78fc7770807a5b3c8493bfb304390a275a31
Ted.H <50784573+cielgrisdemoscou@users.noreply.github.com>
2021-03-21 14:50:13 +0900

    Initial commit

---
+++ one-article/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  one-article
+//
+//  Created by Ted on 2021/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
